212f70b851d22883f72d0f202431a6f953efe105
bnc <1010744256@qq.com>
2019-11-05 20:08:02 +0800

    环境配置

---
+++ src/main/java/api/service/HonorService.java
@@ -0,0 +1,9 @@
+package api.service;
+
+import api.entity.HttpResult;
+
+public interface HonorService {
+
+    HttpResult get(int year);
+
+}
---
+++ src/main/java/api/service/serviceImpl/HonorServiceImpl.java
@@ -0,0 +1,11 @@
+package api.service.serviceImpl;
+
+import api.entity.HttpResult;
+import api.service.HonorService;
+
+public class HonorServiceImpl implements HonorService {
+    @Override
+    public HttpResult get(int year) {
+        return null;
+    }
+}
